783285af5b84c714ca1a1b7ba35c796bb97b98d7
AOI <hatake390o70@yahoo.co.jp>
2018-05-13 14:53:20 +0000

    CRUD messages

---
+++ app/Http/Controllers/MessagesController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\Message;
+
+class MessagesController extends Controller
+{
+    
+     
+  
+     // getでmessages/にアクセスされた場合の「一覧表示処理」
+     //確認済み
+     
+     
+    public function index()
+    {
+        $messages = Message::all();
+        
+    return view ('messages.index',[
+        'messages' => $messages,
+        ]);
+        
+    }
+    
+        
+        
+        
+
+    // getでmessages/createにアクセスされた場合の「新規登録画面表示処理」
+    //確認済み
+    
+    public function create()
+    {
+        $message = new Message;
+        
+        return view('messages.create', [
+            'message' => $message,
+            ]);
+        
+    }
+        
+    
+
+
+    // postでmessages/にアクセスされた場合の「新規登録処理」
+    //確認済み
+    
+    public function store(Request $request)
+    {
+        $message = new Message;
+        $message->content = $request->content;
+        $message->save();
+        
+        return redirect('/');
+        
+    }    
+
+    // getでmessages/idにアクセスされた場合の「取得表示処理」
+    //確認済み
+    
+    public function show($id)
+    {
+        $message = Message::find($id);
+        
+        return view('messages.show', [
+        'message' => $message,
+        ]);
+        
+    }
+    
+    
+    
+    
+    
+    // getでmessages/id/editにアクセスされた場合の「更新画面表示処理」
+    //確認済み
+    
+    public function edit($id)
+    {
+        $message = Message::find($id);
+        
+        return view('messages.edit', [
+            'message' => $message,
+            ]);
+            
+    }
+    
+    
+    
+    // putまたはpatchでmessages/idにアクセスされた場合の「更新処理」
+    //確認済み
+    public function update(Request $request, $id)
+    {
+        
+        $message = Message::find($id);
+        $message->content = $request->content;
+        $message->save();
+
+        return redirect('/');
+        
+    }
+
+
+    // deleteでmessages/idにアクセスされた場合の「削除処理」
+    //確認済み
+    
+    public function destroy($id)
+    {
+         $message = Message::find($id);
+        $message->delete();
+
+        return redirect('/');
+        
+    }
+}
--- config/app.php
+++ config/app.php
@@ -138,6 +138,7 @@ return [
 
     'providers' => [
 
+
         /*
          * Laravel Framework Service Providers...
          */
@@ -176,8 +177,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
-    ],
+        Collective\Html\HtmlServiceProvider::class,
+],
 
     /*
     |--------------------------------------------------------------------------
@@ -225,7 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
-
 ];
